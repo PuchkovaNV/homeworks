56a143632a29b2bc376016f2505c0ce6240d3553
PuchkovaNV <165937815+PuchkovaNV@users.noreply.github.com>
2024-04-14 03:18:06 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+my_string = input('Введите два Ваших любимых цвета (через пробел): ')
+print('Количество символов: ', len(my_string))
+print('Большими буквами: ', my_string.upper())
+print('Маленькими буквами: ', my_string.lower())
+print('Без пробелов: ', my_string.replace(' ',''))
+print('Первая буква: ', my_string[0])
+print('Последняя буква: ', my_string[-1])
+
