5107359ae7e63d662618aad47e9fc0c2049339fc
PuchkovaNV <165937815+PuchkovaNV@users.noreply.github.com>
2024-04-21 14:03:10 +0300

    Add files via upload

---
+++ Homework_for.py
@@ -0,0 +1,8 @@
+list_1 = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in list_1:
+    print('Я езжу на автомобиле марки:', i)
+
+car_count = 0
+for i in list_1:
+    car_count += 10
+    print('Всего машин:', car_count)
