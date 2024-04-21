ea6bc27d63636efb887be20fb8ace87b6a47c5bb
PuchkovaNV <165937815+PuchkovaNV@users.noreply.github.com>
2024-04-21 21:24:54 +0300

    Delete Homework_for.py

--- Homework_for.py
+++
@@ -1,8 +0,0 @@
-list_1 = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in list_1:
-    print('Я езжу на автомобиле марки:', i)
-
-car_count = 0
-for i in list_1:
-    car_count += 10
-    print('Всего машин:', car_count)
