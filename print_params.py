d67a80c27bb79759b90b616eae094f40a4dec1c5
PuchkovaNV <165937815+PuchkovaNV@users.noreply.github.com>
2024-05-08 23:36:02 +0300

    Add files via upload

---
+++ print_params.py
@@ -0,0 +1,20 @@
+def print_params(a, b, c):
+    print(a, b, c)
+
+
+print_params(a=1, b='строка', c=True)
+
+# print_params()  # не работает
+# print_params(5)  # не работает
+# print_params (b = 25)  # не работает
+# print_params(c = [1,2,3])  # не работает
+
+
+values_list = [1, 'anton', True]
+values_dict = {'a': 3, 'b': 'привет', 'c': False}
+
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [7, 'hello']           
+print_params(*values_list_2,42)  # работает
+
